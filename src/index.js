7a61e39e99a70dd468907a0b3cdffdbbb55c0476
Robert <anyrobert@outlook.com.br>
2019-08-19 12:38:12 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
+
+serviceWorker.register();
